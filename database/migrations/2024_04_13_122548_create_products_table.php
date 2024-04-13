b58cbe1a2c8a9f5aadabb6964cc1b9072a4b6d79
xandeact <motive.mobi@gmail.com>
2024-04-13 10:03:26 -0300

    [feat] added model and database migration for product

---
+++ database/migrations/2024_04_13_122548_create_products_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger("code");
+            $table->enum("status", ["draft", "trash", "published"]);
+            $table->dateTimeTz("imported_t");
+            $table->longText("url");
+            $table->string("creator");
+            $table->timestamp("created_t");
+            $table->timestamp("last_modified_t");
+            $table->string("product_name");
+            $table->string("quantity");
+            $table->string("brands");
+            $table->longText("categories");
+            $table->longText("labels");
+            $table->longText("cities")->nullable();
+            $table->longText("purchase_places");
+            $table->string("stores");
+            $table->longText("ingredients_text");
+            $table->longText("traces");
+            $table->string("serving_size");
+            $table->float("serving_quantity");
+            $table->integer("nutriscore_score");
+            $table->string("nutriscore_grade");
+            $table->string("main_category");
+            $table->longText("image_url");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
