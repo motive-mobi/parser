b517d7eab193c476f8dcd4a7e43faedd06a8f768
xandeact <motive.mobi@gmail.com>
2024-04-13 11:28:46 -0300

    [feat] added initial endpoints configuration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,28 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::middleware('auth:sanctum')->group(function () {
+    Route::group(['prefix' => 'v1'], function () {
+
+        Route::get('/', function () {
+            return 'Detalhes da API, se conexão leitura e escritura com a base de dados está OK, horário da última vez que o CRON foi executado, tempo online e uso de memória.';
+        });
+
+        Route::put('/products/{code}', function (Request $request) {
+            return 'Será responsável por receber atualizações do Projeto Web';
+        });
+
+        Route::delete('/products/{code}', function (Request $request) {
+            return 'Mudar o status do produto para trash';
+        });
+
+        Route::get('/products/{code}', function (Request $request) {
+            return 'Obter a informação somente de um produto da base de dados';
+        });
+
+        Route::get('/products', function (Request $request) {
+            return 'Listar todos os produtos da base de dados, adicionar sistema de paginação para não sobrecarregar o REQUEST';
+        });
+
+    });
+//});
